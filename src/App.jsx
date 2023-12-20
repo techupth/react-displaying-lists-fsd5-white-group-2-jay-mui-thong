c3c70b82eb6f92872f4d27ae505fa69398bda096
Jaturapat Dusadee <mr.jaturapat.d@gmail.com>
2023-12-20 15:18:41 +0700

    Finish exercise hackhour

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import "./App.css";
+import movies from "./data/movies";
 
 function App() {
   return (
     <div className="App">
+      <span>Movie Lists Section</span>
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+        {movies.map((item, index) => {
+          return (
+            <div className="box-list-movies" key={index}>
+              <img
+                className="movies-images"
+                src={item.image}
+                alt="movies-image"
+              />
+              <div className="infomation-movies">
+                <span>Title: {item.title}</span>
+                <span>Year: {item.year}</span>
+                <span>Runtime: {item.runtime}</span>
+                <div className="genres-moives-list">
+                  Genres:
+                  {item.genres.map((genre, index) => (
+                    <div className="genres-moives-catagory" key={index}>
+                      {genre}
+                    </div>
+                  ))}
+                </div>
+                <span>IMDB Ratings: {item.imdbRating}</span>
+                <span>IMDB Votes: {item.imdbVotes}</span>
+              </div>
+            </div>
+          );
+        })}
       </section>
     </div>
   );
